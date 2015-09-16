f587bbc717240948a56834dbdee8e701fe54d5b6
Christopher Muller <inbox@christophermuller.net>
2015-09-16 23:28:03 +1000

    Adding test repo for neutroncss

---
+++ Gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+
+// SASS
+gulp.task('sass', function() {
+    return sass('./assets/**/*.scss', {style: 'expanded'})
+	.on('error', sass.logError)
+    .pipe(gulp.dest('assets/css'));
+});
+
+//SASS Watch task
+gulp.task('watch',function() {
+    gulp.watch('./assets/**/*.scss',['sass']);
+});
+
+//Default task
+gulp.task('default', ['sass', 'watch']);
