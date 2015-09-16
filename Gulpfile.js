8e98b13b7b79536202072684e52f0b8f7d9d6909
Christopher Muller <chris.muller@loweprofero.com>
2015-09-17 09:50:31 +1000

    Testing JS concatenation in gulp.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var concat = require('gulp-concat');
+
 
 // SASS
 gulp.task('sass', function() {
@@ -8,10 +10,18 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('assets'));
 });
 
+gulp.task('scripts', function() {
+  return gulp.src('./assets/js/lib/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('assets/js'));
+});
+
+
 //SASS Watch task
 gulp.task('watch',function() {
     gulp.watch('./assets/**/*.scss',['sass']);
+    gulp.watch('./assets/js/lib/*.js',['scripts']);
 });
 
 //Default task
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'scripts', 'watch']);
